dbfb53b4c40081511d6fcee3b79de4e7582496aa
josejesusgarciajr <josejesusgarciajr@gmail.com>
2019-10-14 14:12:43 -0700

    Models

---
+++ Models/ParkDB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFGetStarted.AspNetCore.NewDb.Models
+{
+    public class ParkDB
+    {
+        public int Size { get; set; }
+        public List<Park> parks = new List<Park>();
+
+        public static IEnumerable<Park> Parks
+        {
+            get { return Parks; }
+        }
+
+        public void AddPark(Park p)
+        {
+            this.Size++;
+            parks.Add(p);
+        }
+
+    }
+}
---
+++ Models/Storage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFGetStarted.AspNetCore.NewDb.Models
+{
+    public class Storage
+    {
+        public List<Item> items = new List<Item>();
+        public static IEnumerable<Item> Items
+        {
+            get { return Items; }
+        }
+        public void AddItem(Item thing)
+        {
+            items.Add(thing);
+        }
+    }
+}
