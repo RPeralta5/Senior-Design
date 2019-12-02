71509f7ccf36f28b2a0322a26f8f76c9d5086080
WongProgrammer <francisnguyen2344@gmail.com>
2019-12-02 13:43:28 -0800

    Special Event Registration Form
    Split it up into pages

---
+++ Parks_SpecialEvents/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Parks_SpecialEvents.Models;
+
+namespace Parks_SpecialEvents.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Console.WriteLine("INSIDE HOME CONTROLLER INDEX");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            Console.WriteLine("REGISTRATION PAGE");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
