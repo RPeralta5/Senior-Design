65b1d5bc2dd24788e20f951204f112f769871a9c
josejesusgarciajr <josejesusgarciajr@gmail.com>
2020-01-23 12:24:32 -0800

    Google Calendar StartUp

--- Parks_SpecialEvents/Controllers/AdminController.cs
+++ Parks_SpecialEvents/Controllers/AdminController.cs
@@ -29,7 +29,7 @@ namespace Parks_SpecialEvents.Controllers
         }
 
         // PARKS DATABASE CONNECTION STRING
-        const string PARK_DB_CONNECTION = @"Data Source=LAPTOP-M67PUJ2M;Initial Catalog=parks_faqDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        const string PARK_DB_CONNECTION = @"data source=.; database= PARKS_TEST; user id = sa; password = myPassw0rd";
 
 
         // QUERY FOR ALL PARKS
